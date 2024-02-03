cb722f1fef1a7310e30ee20ef125978019b7326c
Nina Huang <nina.hhhuang@gmail.com>
2024-02-03 17:26:14 -0500

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./src/Login/login.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-color': '#F4F3ED',
+        'logo-color': "#005A5B",
+        'dark-green': '#005A5B',
+        'orange': '#EE6A4C',
+
+    },
+    fontFamily: {
+      'serif': ['Aleo', 'ui-serif', 'Georgia', ...defaultTheme.fontFamily.serif],
+    }
   },
   plugins: [],
 }
 
+}
