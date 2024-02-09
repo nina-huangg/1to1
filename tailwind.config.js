10b185df62053babc56e74ecb19163f3881a3fa5
Jason <jasonli2442@gmail.com>
2024-02-09 00:09:54 -0500

    rename orange alias to orange-primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/Login/login.html", "./src/BookCalendar/index.html"],
+  content: [
+    './src/**/*.{html,js}',
+    './src/Login/login.html',
+    './src/BookCalendar/index.html',
+  ],
   theme: {
     extend: {
       colors: {
         'background-color': '#F4F3ED',
-        'logo-color': "#005A5B",
+        'logo-color': '#005A5B',
         'primary-blue': '#52B7B1',
-        'primary-blue-hover': "#2B928B",
-        'orange': '#EE6A4C',
-        'orange-hover': '#DD4522',
-        'turquoise': '#6EB3AF',
-        'pinkie': '#FBB5A5', // Corrected color definition without extra comma
+        'primary-blue-hover': '#2B928B',
+        orange: { primary: '#EE6A4C', hover: '#DD4522' },
+        turquoise: '#6EB3AF',
+        pinkie: '#FBB5A5', // Corrected color definition without extra comma
+      },
+      fontFamily: {
+        serif: [
+          'Aleo',
+          'ui-serif',
+          'Georgia',
+          ...defaultTheme.fontFamily.serif,
+        ],
+      },
+      plugins: [],
     },
-    fontFamily: {
-      'serif': ['Aleo', 'ui-serif', 'Georgia', ...defaultTheme.fontFamily.serif],
-    }, 
-  plugins: [],
-    }
-  }
+  },
 }
