292a6f7e87312d7e63aa57de73a1dc9c98bd2a3b
thudoan1706 <thudoan1706@gmail.com>
2024-02-08 19:35:14 -0500

    edit the button for the calendar

--- src/BookCalendar/display-confirmed-meetings.js
+++ src/BookCalendar/display-confirmed-meetings.js
@@ -1,4 +1,4 @@
-// calendar.js
+// display-confirmed-meeting.js
 
 function getRandomColor() {
     // Array of predefined colors
@@ -16,24 +16,24 @@ function addMeetingEntry(date, meetingName, meetingTime, meetingLink, attendees)
 
     // Create a new meeting entry HTML structure
     const newMeetingEntry = `
-        <div class="meeting-entry grid grid-cols-8 gap-2 mb-4 ml-10 flex flex-wrap">
-            <div class="meeting-details col-span-6">
-                <div class="date col-start-2 col-span-1 ">
-                    <p class="font-bold">${date}</p>
+        <div class="meeting-entry grid grid-cols-1 md:grid-cols-8 gap-2 mb-4 ml-10 flex flex-wrap">
+        <div class="meeting-details col-span-1 md:col-span-6">
+            <div class="date">
+                <p class="font-bold">${date}</p>
+            </div>
+            <div class="meeting-box grid flex text-sm shadow-xl rounded-md md:w-4/4" style="background-color: ${randomColor};">
+                <div class="info-right-col ml-2 m-2 pr-8" style="flex-direction: column;">
+                    <p class="meeting-name text-base">${meetingName}</p>
+                    <p class="meeting-time text-xs">Time: ${meetingTime}</p>
+                    <p class="meeting-link text-xs">${meetingLink}</p>
                 </div>
-                <div class="meeting-box grid flex text-sm shadow-xl rounded-md w-4/4" style="background-color: ${randomColor};">
-                    <div class="info-right-col ml-4 m-2 pr-14">
-                        <p class="meeting-name">${meetingName}</p>
-                        <p class="meeting-time">Time: ${meetingTime}</p>
-                        <p class="meeting-link">${meetingLink}</p>
-                    </div>
-                    <div class="info-left-col pl-14">
-                        <img src="setting.png" class="w-7 h-7 ml-14" alt="Settings Icon">
-                        <p class="attendees mt-3 ml-5 pt-2">${attendees}</p>
-                    </div>
+                <div class="info-left-col pl-12">
+                    <img src="setting.png" class="flex w-7 h-7 ml-14" alt="Settings Icon">
+                    <p class="attendees text-xs mt-3 ml-5 pt-2">${attendees}</p>
                 </div>
             </div>
         </div>
+    </div>
     `;
 
     document.querySelector('.left-column').insertAdjacentHTML('beforeend', newMeetingEntry);
