4840e97e78bb0143091835196645280eeb5e8b6c
Nina Huang <nina.hhhuang@gmail.com>
2024-03-12 09:21:45 -0400

    start up scripts

---
+++ startup.sh
@@ -0,0 +1,21 @@
+# installing python interpreter
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv libjpeg-dev
+
+# activating virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# installing django
+pip install django
+pip install djangorestframework-simplejwt
+
+#move to correct folder 
+cd backend
+
+# run migrations
+python manage.py migrate
+
+
+
+
