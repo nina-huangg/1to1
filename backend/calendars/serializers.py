f81f3bcebf3f7bcc4233612b8eded7a19d8a3f7f
Jason <lijason.li@mail.utoronto.ca>
2024-03-13 01:55:37 -0400

    add serializers for new models

--- backend/calendars/serializers.py
+++ backend/calendars/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
-from .models import Calendar, Availability
+from .models import Calendar, Availability, Invitation, Meeting, BlockedTime
 
 
 class AvailabilitySerializer(serializers.ModelSerializer):
 
     preference = serializers.ChoiceField(
-        choices=Availability.PREFERENCE_CHOICES, default=Availability.HIGH_PREFERENCE
+        choices=Availability.PREFERENCE_CHOICES,
+        default=Availability.MEDIUM_PREFERENCE,
     )
 
     class Meta:
         model = Availability
-        fields = ("id", "date", "start_time", "end_time", "preference", "owner")
+        fields = ("id", "date", "start_time", "end_time", "preference", "invitation")
 
 
 class CalendarSerializer(serializers.ModelSerializer):
@@ -20,3 +21,38 @@ class CalendarSerializer(serializers.ModelSerializer):
         model = Calendar
         fields = ("id", "name", "description", "availability_set")
         depth = 1
+
+
+class InvitationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Invitation
+        fields = (
+            "id",
+            "invitee",
+            "confirmed",
+        )
+
+
+class MeetingSerializer(serializers.ModelSerializer):
+    invitation_set = InvitationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Meeting
+        fields = (
+            "id",
+            "name",
+            "description",
+            "duration",
+            "last_modified",
+            "start_date",
+            "end_date",
+            "invitation_set",
+        )
+
+
+class BlockedTimeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BlockedTime
+        fields = ("id", "start_time", "end_time", "meeting")
