9508dd6eb6d71659fa2b2bc00caf2ba1322d06b6
Jason <lijason.li@mail.utoronto.ca>
2024-03-13 01:55:25 -0400

    add invitation model and change existing models

--- backend/calendars/models.py
+++ backend/calendars/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Availability(models.Model):
     HIGH_PREFERENCE = "high"
@@ -18,16 +16,32 @@ class Availability(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    calendar = models.ForeignKey("Calendar", on_delete=models.CASCADE)
-
-
-# class Invitation(models.Model):
-#     number = models.IntegerField()
+    invitation = models.ForeignKey("Invitation", on_delete=models.CASCADE)
 
 
 class Calendar(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    # invitation = models.ForeignKey(Invitation, on_delete=models.CASCADE, related_name='invitations')
+
+
+class Meeting(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+    duration = models.DurationField()
+    last_modified = models.DateTimeField(auto_now=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class BlockedTime(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+
+
+class Invitation(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    invitee = models.ForeignKey(User, on_delete=models.CASCADE)
+    confirmed = models.BooleanField()
