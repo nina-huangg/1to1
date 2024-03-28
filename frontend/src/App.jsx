3df1f8a2329832fb477cadcac03db864b480ebb3
thudoan1706 <thudoan1706@gmail.com>
2024-03-28 14:52:35 -0400

    init project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import LandingPage from "./pages/LandingPage"
+import ProtectedRoute from "./utils/ProtectedRoute"
+import './App.css'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+function LandingPage() {
+    return <h1 className="text-3xl font-bold underline">
+      This is Landing Page With TailWind CSS Style
+    </h1>
+
+}
+
+export default LandingPage;
