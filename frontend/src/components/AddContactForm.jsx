f695514e66ef9a73753f843b2a2adc2e9624f68a
anushkasharma <aanushka.sharma@mail.utoronto.ca>
2024-04-09 19:06:04 -0400

    remove output.css import

--- frontend/src/components/AddContactForm.jsx
+++ frontend/src/components/AddContactForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api';
 import '../styles/Contacts_stye.css';
-import '../output.css';
 
 function NewContactPage() {
   const [formData, setFormData] = useState({
