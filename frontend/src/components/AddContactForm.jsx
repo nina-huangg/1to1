d7b5ace119bec65c7b3f1a9dc94f8ae24477a054
Nina Huang <nina.hhhuang@gmail.com>
2024-04-09 22:58:34 -0400

    fixed styling

--- frontend/src/components/AddContactForm.jsx
+++ frontend/src/components/AddContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api';
+import Header from './Header';
 import '../styles/Contacts_stye.css';
 
 function NewContactPage() {
@@ -47,45 +48,7 @@ function NewContactPage() {
 
   return (
     <div className="bg-background-color">
-      <header className="h-30px xl:h-60px lg:h-60px md:h-40px sm:h-40px">
-                <div className="flex flex-row items-center justify-between">
-                    <a className="xl:w-40 lg:w-40 md:w-32 sm:w-32 w-28 xl:h-12 lg:h-12 md:h-10 sm:h-10 h-8 
-                        xl:px-8 lg:px-8 md:px-6 sm:px-6 px-4 xl:py-2.5 lg:py-2.5 md:py-1.5 sm:py-1.5 py-1.5 xl:mx-10 lg:mx-10 md:mx-7 sm:mx-7
-                        mx-4 my-5 xl:text-xl lg:text-xl md:text-lg sm:text-lg text-sm rounded-full text-center align-middle shadow-lg
-                        shadow-gray-400 bg-logo-color text-white" href="">
-                        1 ON 1
-                    </a>
-                    <div className="align-right">
-                        <a className="xl:w-30 lg:w-30 md:w-24 sm:w-20 w-14 xl:h-9 lg:h-9 md:h-7 sm:h-7 h-5 
-                            xl:px-8 lg:px-8 md:px-6 sm:px-4 px-2 py-2 
-                            xl:mr-5 lg:mr-5 md:mr-4 sm:mr-3 mr-2 my-5 
-                            xl:text-l lg:text-l md:text-sm sm:text-sm text-xs rounded-full text-center align-middle 
-                            shadow-lg shadow-gray-400 hover:bg-primary-blue-hover bg-primary-blue text-white"
-                            href="../MyMeetings/index.html">
-                            My Meetings
-                        </a>
-
-                        <a className="xl:w-30 lg:w-30 md:w-24 sm:w-20 w-14 xl:h-9 lg:h-9 md:h-7 sm:h-7 h-5 
-                            xl:px-8 lg:px-8 md:px-6 sm:px-4 px-2 py-2 
-                            xl:mr-5 lg:mr-5 md:mr-4 sm:mr-3 mr-2 my-5 
-                            xl:text-l lg:text-l md:text-sm sm:text-sm text-xs rounded-full text-center align-middle 
-                            shadow-lg shadow-gray-400 hover:bg-primary-blue-hover bg-primary-blue text-white"
-                            href="../MyContacts/Contacts_index.html">
-                            My Contacts
-                        </a>
-
-                        <a className="xl:w-30 lg:w-30 md:w-24 sm:w-20 w-14 xl:h-9 lg:h-9 md:h-7 sm:h-7 h-5 
-                            xl:px-8 lg:px-8 md:px-6 sm:px-4 px-2 py-2 
-                            xl:mr-8 lg:mr-8 md:mr-6 sm:mr-4 mr-2 my-5 
-                            xl:inline lg:inline md:inline sm:inline hidden
-                            xl:text-l lg:text-l md:text-sm sm:text-sm text-xs rounded-full text-center align-middle 
-                            shadow-lg shadow-gray-400 border-solid border-0 border-gray-500 bg-orange hover:bg-orange-hover text-white"
-                            href="../Profile/profile.html">
-                            Profile
-                        </a>
-                    </div>
-                </div>
-            </header>
+      <Header/>
 
 
       <section className="main-section">
@@ -98,7 +61,7 @@ function NewContactPage() {
             <input type="file" id="imageUpload" name="image" accept="image/*" onChange={handleChange} />
             <img id="contactImage" className="profile-image" />
 
-            <form onSubmit={handleSubmit} id="contactForm">
+            <form className='edit_form' onSubmit={handleSubmit} id="contactForm">
               <label htmlFor="firstName">First Name:</label>
               <input type="text" id="firstName" name="first_name" onChange={handleChange} required />
 
--- frontend/src/components/MeetingCard.jsx
+++ frontend/src/components/MeetingCard.jsx
@@ -16,7 +16,7 @@ const MeetingCard = ({ meet }) => {
     
             {/* <div className="absolute bottom-4 right-4">
                 
-                <button  onClick={() => onDelete(calendar.id)} className="bg-orange hover:bg-turquoise text-white font-semibold py-1 px-1 ml-3 rounded text-xs">
+                <button  onClick={() => handleDelete(meet.id)} className="bg-orange hover:bg-turquoise text-white font-semibold py-1 px-1 ml-3 rounded text-xs">
                     Delete Meeting
                 </button>
             </div> */}
