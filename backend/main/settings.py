b62f78d13a7a9dce21a3419f2ad92d96db9c163d
thudoan1706 <thudoan1706@gmail.com>
2024-04-11 23:05:06 -0400

    update

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 2587
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
