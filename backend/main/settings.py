1fd59f78827724cf8e8ca923260adf61a05df015
thudoan1706 <thudoan1706@gmail.com>
2024-04-12 02:34:55 -0400

    update

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -170,8 +170,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1to1',
-        'USER': 'some',
-        'PASSWORD':  'some',
+        'USER': 'admin1da906z',
+        'PASSWORD':  'yJn89VDHyf8YArlbsq2y',
         'HOST': 'localhost',
         'PORT': ''
     }
