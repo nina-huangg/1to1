2e5603a9aaf80935a324fc9aff9fb120277adbe0
thudoan1706 <thudoan1706@gmail.com>
2024-04-11 22:11:36 -0400

    update

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -148,11 +148,11 @@ WSGI_APPLICATION = "main.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
